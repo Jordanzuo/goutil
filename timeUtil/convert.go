595743bf4071c2772727cdd6b9980f139657ccf3
zuoxianqing@moqikaka.com <zuoxianqing@moqikaka.com>
2016-07-13 20:38:27 +0800

    优化了timeUtil中ConverToStandardFormat方法的返回值类型

--- timeUtil/convert.go
+++ timeUtil/convert.go
@@ -11,14 +11,14 @@ import (
 // str：输入，格式为：2015-10-25T17:07:30
 // 返回值：
 // 标准时间格式对象
-func ConverToStandardFormat(str string) time.Time {
+func ConverToStandardFormat(str string) (time.Time, error) {
 	str = strings.Replace(str, "T", ":", -1)
 	str = strings.Replace(str, "-", ":", -1)
 	slice := strings.Split(str, ":")
 	intSlice := make([]int, len(slice))
 	for index, item := range slice {
 		if intItem, err := strconv.Atoi(item); err != nil {
-			panic(fmt.Errorf("输入字符串的格式错误:%s", str))
+			return time.Now(), fmt.Errorf("输入字符串的格式错误:%s", str)
 		} else {
 			intSlice[index] = intItem
 		}
@@ -26,10 +26,10 @@ func ConverToStandardFormat(str string) time.Time {
 
 	// 检查数量
 	if len(intSlice) != 6 {
-		panic(fmt.Errorf("输入字符串的格式错误:%s", str))
+		return time.Now(), fmt.Errorf("输入字符串的格式错误:%s", str)
 	}
 
-	return time.Date(intSlice[0], time.Month(intSlice[1]), intSlice[2], intSlice[3], intSlice[4], intSlice[5], 0, time.Local)
+	return time.Date(intSlice[0], time.Month(intSlice[1]), intSlice[2], intSlice[3], intSlice[4], intSlice[5], 0, time.Local), nil
 }
 
 // 将时间转换为int类型（20160120，共8位）
