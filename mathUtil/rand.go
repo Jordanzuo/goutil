07aa1a8c4d1224751615e612d2525003dd93584d
左贤清 <zuoxianqing@moqikaka.com>
2016-12-02 20:32:20 +0800

    完善了随机算法

--- mathUtil/rand.go
+++ mathUtil/rand.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-// 获得Rand对象
+// 获得Rand对象(如果是循环生成多个数据，则只需要调用本方法一次，而不能调用多次，否则会得到相同的随机值)
 func getRand() *rand.Rand {
 	return rand.New(rand.NewSource(time.Now().UnixNano()))
 }
@@ -72,29 +72,23 @@ func GetRandNumList(minValue, maxValue, count int, ifAllowDuplicate bool) ([]int
 		return nil, errors.New("随机数的区间不能大于10000")
 	}
 
-	// 定义返回值
-	resultList := make([]int, 0, count)
-	var err error
-
-	// 如果允许重复，则直接随机；否则调用GetRandList来随机
-	if ifAllowDuplicate {
-		for {
-			if len(resultList) < count {
-				resultList = append(resultList, GetRandRangeInt(minValue, maxValue+1))
-			} else {
-				break
-			}
-		}
-	} else {
-		inSlice := make([]int, maxValue-minValue+1, maxValue-minValue+1)
-		for index := 0; index < len(inSlice); index++ {
-			inSlice[index] = minValue + index
-		}
-
-		resultList, err = GetRandIntList(inSlice, count, ifAllowDuplicate)
+	// 定义原始数据
+	sourceCount := maxValue - minValue + 1
+	source := make([]int, sourceCount, sourceCount)
+	for index := 0; index < sourceCount; index++ {
+		source[index] = minValue + index
 	}
 
-	return resultList, err
+	// 定义返回值
+	resultList := make([]int, 0, count)
+
+	// 获取随机的索引列表
+	randIndextList := getRandIndexList(count, ifAllowDuplicate)
+	for _, index := range randIndextList {
+		resultList = append(resultList, source[index])
+	}
+
+	return resultList, nil
 }
 
 // 获取随机的int列表
@@ -109,41 +103,13 @@ func GetRandIntList(source []int, count int, ifAllowDuplicate bool) ([]int, erro
 		return nil, errors.New("随机的数量超过列表的元素数量")
 	}
 
-	// 使用源列表的数据量来初始化一个仅存放索引值的数组
-	indexList := make([]int, count, count)
-	for index := 0; index < count; index++ {
-		indexList[index] = index
-	}
-
 	// 定义返回值
 	resultList := make([]int, 0, count)
 
-	// 遍历列表并获取随机对象(通过不断缩小随机的范围来实现)
-	maxIndex := len(indexList) - 1
-	for {
-		if len(resultList) < count {
-			// 获取随机索引(由于Next方法不取上限值，所以需要maxIndex+1)
-			randIndex := GetRandRangeInt(0, maxIndex+1)
-
-			// 将数据添加到列表，并增加findCount
-			resultList = append(resultList, source[indexList[randIndex]])
-
-			// 如果不允许重复，则需要特殊处理
-			if !ifAllowDuplicate {
-				// C#版本
-				// // 并将该位置的数据设置为当前遍历的最大值
-				// indexList[randIndex] = indexList[maxIndex]
-
-				// Go版本
-				// 将该位置的数据和最大位置的数据进行交换
-				indexList[randIndex], indexList[maxIndex] = indexList[maxIndex], indexList[randIndex]
-
-				// 将随机的范围缩小
-				maxIndex = maxIndex - 1
-			}
-		} else {
-			break
-		}
+	// 获取随机的索引列表
+	randIndextList := getRandIndexList(count, ifAllowDuplicate)
+	for _, index := range randIndextList {
+		resultList = append(resultList, source[index])
 	}
 
 	return resultList, nil
@@ -161,42 +127,58 @@ func GetRandInterfaceList(source []interface{}, count int, ifAllowDuplicate bool
 		return nil, errors.New("随机的数量超过列表的元素数量")
 	}
 
+	// 定义返回值
+	resultList := make([]interface{}, 0, count)
+
+	// 获取随机的索引列表
+	randIndextList := getRandIndexList(count, ifAllowDuplicate)
+	for _, index := range randIndextList {
+		resultList = append(resultList, source[index])
+	}
+
+	return resultList, nil
+}
+
+// 获取随机的索引列表
+// count:数量
+// ifAllowDuplicate:是否允许重复
+// 返回值
+// 随机索引值列表
+func getRandIndexList(count int, ifAllowDuplicate bool) []int {
+	// 定义返回值
+	randIndextList := make([]int, 0, count)
+
 	// 使用源列表的数据量来初始化一个仅存放索引值的数组
 	indexList := make([]int, count, count)
 	for index := 0; index < count; index++ {
 		indexList[index] = index
 	}
 
-	// 定义返回值
-	resultList := make([]interface{}, 0, count)
+	// 定义随机变量
+	randObj := getRand()
 
 	// 遍历列表并获取随机对象(通过不断缩小随机的范围来实现)
 	maxIndex := len(indexList) - 1
 	for {
-		if len(resultList) < count {
+		if len(randIndextList) < count {
 			// 获取随机索引(由于Next方法不取上限值，所以需要maxIndex+1)
-			randIndex := GetRandRangeInt(0, maxIndex+1)
+			randIndex := randObj.Intn(maxIndex + 1)
 
 			// 将数据添加到列表，并增加findCount
-			resultList = append(resultList, source[indexList[randIndex]])
+			randIndextList = append(randIndextList, indexList[randIndex])
 
 			// 如果不允许重复，则需要特殊处理
 			if !ifAllowDuplicate {
-				// C#版本
-				// // 并将该位置的数据设置为当前遍历的最大值
-				// indexList[randIndex] = indexList[maxIndex]
-
-				// Go版本
 				// 将该位置的数据和最大位置的数据进行交换
 				indexList[randIndex], indexList[maxIndex] = indexList[maxIndex], indexList[randIndex]
 
 				// 将随机的范围缩小
-				maxIndex = maxIndex - 1
+				maxIndex -= 1
 			}
 		} else {
 			break
 		}
 	}
 
-	return resultList, nil
+	return randIndextList
 }
