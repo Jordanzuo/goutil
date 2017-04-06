b74e52b5a78be2f031e4cef6b6157c882f2e13b9
左贤清 <zuoxianqing@moqikaka.com>
2017-04-06 13:59:32 +0800

    增加了获取net.Conn的Ip、Port的逻辑

---
+++ netUtil/net.go
@@ -0,0 +1,30 @@
+package netUtil
+
+import (
+	"net"
+	"strconv"
+	"strings"
+)
+
+// 获取远程Ip
+func GetRemoteIp(conn net.Conn) string {
+	items := strings.Split(conn.RemoteAddr().String(), ":")
+
+	return items[0]
+}
+
+// 获取远程Ip和Port
+func GetRemoteIpAndPort(conn net.Conn) (string, int) {
+	items := strings.Split(conn.RemoteAddr().String(), ":")
+	ip := items[0]
+	port, _ := strconv.Atoi(items[1])
+
+	return ip, port
+}
+
+// 获取远程地址（格式：Ip_Port）
+func GetRemoteAddr(conn net.Conn) string {
+	items := strings.Split(conn.RemoteAddr().String(), ":")
+
+	return fmt.Sprintf("%s_%s", items[0], items[1])
+}
